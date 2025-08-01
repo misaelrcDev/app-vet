65d7bf99c5f15031c2c5ba676fd96d21c004f5b9
Misael Cabral <misaelrc.dev@gmail.com>
2025-08-01 16:15:46 -0300

    Adicionado icones em Tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
 import { Tabs } from "expo-router";
 import { Platform } from "react-native";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function TabLayout() {
   return (
@@ -13,19 +14,22 @@ export default function TabLayout() {
       <Tabs.Screen 
         name="index"
         options={{ 
-          title: 'Home'
+          title: 'Home',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
          }}
       />
       <Tabs.Screen 
         name="tasks"
         options={{ 
-          title: 'Tasks'
+          title: 'Tasks',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="check" color={color} />,
          }}
       />
       <Tabs.Screen 
         name="counter"
         options={{ 
-          title: 'Counter'
+          title: 'Counter',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="hourglass" color={color} />,
          }}
       />
     </Tabs>
