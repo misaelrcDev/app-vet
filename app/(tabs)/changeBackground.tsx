b08adaa5103670b2c4c2aa73e788078e8b619ef2
Misael Cabral <misaelrc.dev@gmail.com>
2025-08-02 10:28:25 -0300

    Criado a logica de changeBackground

--- app/(tabs)/changeBackground.tsx
+++ app/(tabs)/changeBackground.tsx
@@ -1,10 +1,51 @@
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
-import { useState } from 'react';
+import { MaterialIcons } from "@expo/vector-icons";
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function ChangeBackground() {
-    return (
-        <View>
-            <Text>Cor Atual: </Text>
-        </View>
-    )
+  const [background, setBackground] = useState("");
+
+  const colors = [
+    "#FFC0CB", // Rosa
+    "#ADD8E6", // Azul Claro
+    "#90EE90", // Verde Claro
+    "#FFD700", // Dourado
+    "#FFA07A", // Salmão Claro
+    "#DDA0DD", // Ameixa
+    "#AFEEEE", // Turquesa Claro
+    "#FF6347", // Tomate
+    "#F0E68C", // Caqui
+    "#DA70D6", // Orquídea
+  ];
+
+  const generateBackgroundColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  }
+
+  const changeBackgroundColor = () => {
+    const newColor = generateBackgroundColor();
+    // console.log(newColor);
+    setBackground(newColor);
+  }
+
+  return (
+    <View style={[styles.container, {backgroundColor: background}]}>
+      <Text style={styles.text}>Cor Atual: {background}</Text>
+      <TouchableOpacity onPress={changeBackgroundColor}>
+        <MaterialIcons name="change-circle" size={68} color="blue" />
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 25,
+  },
+});
